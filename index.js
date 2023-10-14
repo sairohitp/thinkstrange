0bb4c3abb5c35a08bd61eae6ba3f732a50d46f4d
Sai Rohit <sairohitp007@gmail.com>
2023-10-14 22:56:03 +0530

    commit

--- index.js
+++ index.js
@@ -4,3 +4,33 @@ document.addEventListener("DOMContentLoaded", (e) => {
         splash.classList.add("display-none");
     }, 1000);
 })
+
+const faqItem = document.querySelectorAll(".faqitem")
+
+faqItem.forEach(item => {
+    const question = item.querySelector(".faqquestion")
+    const answer = item.nextElementSibling
+    const icon = item.querySelector("i")
+
+    item.addEventListener("click", () => {
+        faqItem.forEach(otherItem => {
+            if (otherItem !== item) {
+                const otherAnswer = otherItem.nextElementSibling
+                const otherIcon = otherItem.querySelector("i")
+
+                otherAnswer.classList.remove("active")
+                otherIcon.classList.remove("active")
+                otherAnswer.style.maxHeight = "0";                
+            }
+        })
+
+        answer.classList.toggle("active")
+        icon.classList.toggle("active")
+
+        if (answer.classList.contains("active")) {
+            answer.style.maxHeight = answer.scrollHeight + "px"
+        } else {
+            answer.style.maxHeight = "0"
+        }
+    })
+})
